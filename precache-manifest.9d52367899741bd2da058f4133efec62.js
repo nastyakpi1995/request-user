bd9124d55af23ec00ca8e28a54abb8c70c224ff5
Nastya <nastyakpi1995@gmail.com>
2020-05-29 14:25:33 +0300

    Updates

---
+++ precache-manifest.9d52367899741bd2da058f4133efec62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "785f8e805396f02f2409c10b7fd654af",
+    "url": "/request-user/index.html"
+  },
+  {
+    "revision": "dac18ba74ad2fefd40d3",
+    "url": "/request-user/static/js/2.3ef3c23d.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/request-user/static/js/2.3ef3c23d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c9221853a5ca378cb9c",
+    "url": "/request-user/static/js/main.f335f2ff.chunk.js"
+  },
+  {
+    "revision": "4f527b3d4a429f69f557",
+    "url": "/request-user/static/js/runtime-main.24165406.js"
+  }
+]);
